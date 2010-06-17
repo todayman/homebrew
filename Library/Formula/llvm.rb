d700d167cca06b336281d4306d65cf8f9378e9a0
Paul O'Neil <redballoon36@gmail.com>
2010-06-17 01:01:25 -0400

    Added --HEAD and --debug to llvm formula

--- Library/Formula/llvm.rb
+++ Library/Formula/llvm.rb
@@ -1,37 +1,73 @@
 require 'formula'
 
 def build_clang?; ARGV.include? '--with-clang'; end
+def build_debug?; ARGV.include? '--debug'; end
 
 class Clang <Formula
   url       'http://llvm.org/releases/2.7/clang-2.7.tgz'
+  head	    'http://llvm.org/svn/llvm-project/cfe/trunk'
   homepage  'http://llvm.org/'
   md5       'b83260aa8c13494adf8978b5f238bf1b'
+  def download_strategy
+    if version == 'HEAD'
+      SubversionDownloadStrategy
+    else
+      CurlDownloadStrategy
+    end
+  end
 end
 
 class Llvm <Formula
   url       'http://llvm.org/releases/2.7/llvm-2.7.tgz'
+  head	    'http://llvm.org/svn/llvm-project/llvm/trunk'
   homepage  'http://llvm.org/'
   md5       'ac322661f20e7d6c810b1869f886ad9b'
+  def download_strategy
+    if version == 'HEAD'
+      SubversionDownloadStrategy
+    else
+      CurlDownloadStrategy
+    end
+  end
 
   def options
     [
-      ['--with-clang', 'Also build & install clang']
+      ['--with-clang', 'Also build & install clang'],
+      ['--debug', 'Build LLVM ( and clang perhaps ) with debugging symbols']
     ]
   end
 
   def install
     ENV.gcc_4_2 # llvm can't compile itself
-
+    
+    ohai 'trying out this ohai thing'
+    
     if build_clang?
       clang_dir = Pathname.new(Dir.pwd)+'tools/clang'
-      Clang.new.brew do
+      if version == 'HEAD'
+        clang_form = Clang.new 'clang'
+      else
+        clang_form = Clang.new
+      end
+      clang_form.brew do
+        puts 'Dir star is:'
+        puts Dir['*']
         clang_dir.install Dir['*']
+        puts 'put clang into the llvm build tree'
       end
     end
-
-    system "./configure", "--prefix=#{prefix}",
-                          "--enable-targets=host-only",
-                          "--enable-optimized"
+    
+    puts 'about to configure llvm'
+    if build_debug?
+      system "./configure", "--prefix=#{prefix}",
+                            "--enable-debug-runtime",
+                            "--enable-debug-symbols"
+    else
+      system "./configure", "--prefix=#{prefix}",
+                            "--enable-targets=host-only",
+                            "--enable-optimized"
+    end
+      
     system "make"
     system "make install" # seperate steps required, otherwise the build fails
 
