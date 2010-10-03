c9299253d4f476c4d3852f8d7696e24bee15cf49
Paul O'Neil <redballoon36@gmail.com>
2010-10-03 16:36:51 -0400

    flam3 itself needs tinkering in configure and Makefiles, but here's the formula

--- Library/Formula/flam3.rb
+++ Library/Formula/flam3.rb
@@ -2,12 +2,11 @@ require 'formula'
 
 class Flam3 <Formula
   url 'http://flam3.googlecode.com/svn/trunk',
-    :using =>SubversionDownloadStrategy, :revision => '17'
+    :using =>SubversionDownloadStrategy, :revision => '19'
   homepage 'http://code.google.com/p/flam3/'
-  version '2.8b10'
+  version '2.8b11'
   
   def install
-    ohai "hello!"
     system "./src/configure", "--disable-dependency-tracking",
       "--prefix=#{prefix}", "--enable-pthread",
       "--enable-atomic-ops", "--enable-shared", "--enable-static"
