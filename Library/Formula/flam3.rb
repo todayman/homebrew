431b0502e5323edf9c64aa7f751df8eb881bdd6d
Paul O'Neil <redballoon36@gmail.com>
2010-10-07 00:55:26 -0400

    flam3 version to 3.0 - works now

--- Library/Formula/flam3.rb
+++ Library/Formula/flam3.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class Flam3 <Formula
   url 'http://flam3.googlecode.com/svn/trunk',
-    :using =>SubversionDownloadStrategy, :revision => '19'
+    :using =>SubversionDownloadStrategy, :revision => '24'
   homepage 'http://code.google.com/p/flam3/'
-  version '2.8b11'
+  version '3.0'
   
   def install
     system "./src/configure", "--disable-dependency-tracking",
